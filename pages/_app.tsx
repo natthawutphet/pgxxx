d44c14f0afd4d023303355e5e12b356f9c2793a2
xxx <xnatthawut1990@gmail.com>
2024-03-02 10:52:38 +0700

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+import type { AppProps } from 'next/app';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,255 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+
+    <Head>
+    <title> PGเว็บตรงมั่นคง ปลอดภัย 100%  มีระบบการเงินที่เชื่อถือได้ มั่นคง ปลอดภัย 100% สามารถการันตีได้เลยว่ามีความมั่นคงทางการเงิน 100%</title>
+    <meta name="description" content="PGเว็บตรงมั่นคง มีระบบการเงินที่เชื่อถือได้ มั่นคง ปลอดภัย 100% สามารถการันตีได้เลยว่ามีความมั่นคงทางการเงิน 100%" />
+    <meta property="og:image" content="/all/pro01.jpg"/>
+    <meta property="og:site_name" content="PGเว็บตรงมั่นคง"/> 
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+    </Head>
+
+
+<div className="container-fluid">
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Navbar w/ text</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" href="#">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="#">Features</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="#">Pricing</Link>
+          </li>
+        </ul>
+        <span className="navbar-text">
+          Navbar text with an inline element
+        </span>
+      </div>
+    </div>
+  </nav>
+ </div>
+    
+
+<header className="container-fluid"> 
+
+<div className="topa"> 
+
+  
+       <img src="/all/pro01.jpg" alt="" />
+       <img src="/all/pro02.jpg" alt="" />
+
+       </div>
+<div className="topa"> 
+  
+       <img src="/all/pro03.jpg" alt="" />
+       <img src="/all/pro04.jpg" alt="" />
+
+       </div>
+       <div className="topimg">
+       <img src="/all/t1.png" alt="" />
+       <img src="/all/t2.png" alt="" />
+       <img src="/all/t3.png" alt="" />
+       </div>
+  
+
+</header>
+
+<div className="btn-top text-center">
+  <Link href="/" className="btn-top"><img src="/all/login.png" alt="" /></Link>
+<Link href="/" className="btn-top"><img src="/all/btn.png" alt="" /></Link>
+</div>
+
+<section className="container-fluid" >
+  <div className="pg">
+    <div className="pga">
+     <div className="text-center">
+      <div className="image-container">
+        <img src="/all/banner-content-vertical.png" className="imgpg" alt="PG Slot"  />
+        
+        </div>
+     </div>
+
+    </div>
+  
+    <div className="pga">
+      <div className="text-center">
+     
+        <img src="/all/logo.png" alt="PG Slot"  />
+   
+       <h1>PG เว็บตรงไม่ผ่านเอเย้น100%</h1>
+       <h3> เว็บพนันมั่นคงอันดับ 1 ในไทย
+      </h3>
+<p>มีระบบการเงินที่เชื่อถือได้ มั่นคง ปลอดภัย 100% สามารถการันตีได้เลยว่ามีความมั่นคงทางการเงิน 100%
+
+  เว็บสล็อต UFABET.COM คาสิโนออนไลน์ เราคือเว็บผู้ให้บริการหลักของเว็บสล็อตออนไลน์ชั้นนำระดับประเทศ รวบรวมเว็บสล็อตทั้งไทยและเทศ มาไว้ที่เดียว UFABET.COM ทุกเว็บการันตีด้วยระบบพนันระดับมาตรฐานสากล ออกแบบระบบให้เข้าใจง่าย ใช้งานได้ทุกเพศทุกวัย รองรับการเล่นทั้งเล่นผ่านคอมพิวเตอร์ และมือถือทุกระบบ UFABET.COM มีเกมส์ให้เลือกหลายค่ายดังอาทิเช่น SAgame, SAgaming, Sexy Baccarat, Pretty Gaming, AG Gaming, PGSLOT มากมาย ภาพเกมส์แบบใหม่ที่ภาพกราฟฟิคสวยงามสมจริง สามารถเล่นผ่านเว็บบราวเซอร์ ติดตั้งบนคอมพิวเตอร์ หรือเล่นผ่านมือถือได้ทุกที่ทุกเวลาเพียงท่านสมัครสมาชิก สามารถโหลดแอพมาลงไว้เพื่อเล่นได้ในมือถือทั้ง IOS และ Android อยากเล่นเกมส์ ยิงปลา เกมส์สล็อต เกมส์คาสิโน และเกมส์ต่างๆมีให้เลือกกว่า 100 เกมส์
+  
+  หากท่านมีข้อสงสัยหรือต้องการคำปรึกษากรุณาติดต่อสอบถาม CALL CENTER ได้ตลอด 24 ชั่วโมง. หรือ ทักแชทไลน์มาได้เลย
+  
+  </p>
+
+
+    </div>
+    </div>
+  </div>
+
+</section>
+
+<main className="text-center" >
+<h1>PG เว็บตรงมั่นปลอดภัย100% PG Gameเว็บตรง</h1>
+
+
+<img src="/all/logo.png" alt="PG Slot"  />
+  
+
+<h1>PG Game เว็บใหญ่ ค่ายตรง</h1>
+้้<h3>
+  PG แจกเครดิตฟรี 300 บาท ไม่ต้อง แชร์แจกจริงทุกยุสเซอร์
+
+</h3>
+
+<p className="w600" >
+  เล่นตรงกับบริษัทยูฟ่าเบท PG Gameเว็บตรง ไม่ผ่านเอเย่นต์
+  PG Gameเว็บตรง เว็บตรงของบริษัทPG Gameเว็บตรง ทางเข้าสู่ระบบ เว็บแทงบอลออนไลน์ บอลเดี่ยว สเต็ป บอลชุด เดิมพันกีฬาทุกประเภท เล่นคาสิโนออนไลน์ คาสิโนสด เกมยิงปลา สล็อตออนไลน์ เกมไพ่ออนไลน์ทุกรูปแบบ เล่นบาคาร่า รูเล็ต ไฮโล เกมคาสิโนยอดนิยมหลากหลาย ด้วยระบบฝากถอนออโต้ ทันสมัย ปลอดภัย แจกเครดิตฟรีทุกวัน สมัครสมาชิกยูฟ่าเบท ติดต่อเราได้ง่ายๆผ่าน เว็บหลักที่นี่เปิดให้บริการตลอด 24 ชั่วโมง
+
+
+<img src="/img/1 (1).gif" alt="PG Slot"  /></p>
+<img src="/all/logo.png" alt="PG Slot"  />
+
+
+<h1>PG Gameเว็บตรง แทงบอล กีฬาออนไลน์</h1>
+<p className="w600">
+  เว็บแทงบอลออนไลน์ ยูฟ่าเบท แทงบอลขั้นต่ำ 10 บาท แทงบอลลีกเล็ก ลีกใหญ่ ในการแทงบอลสเต็ป บอลเต็ง บอลไลฟ์ ดูบอลสดฟรีของเราไม่ต้องเสียค่าสมัคร พนันบอลออนไลน์ได้ราคาต่อรองดีที่สุดในประเทศไทย ค่าน้ำ 4 ตังค์ ค่าคอม 0.5-1% อัตราต่อรองหลากหลาย Asian Handicap (ต่อลูก) สูง-ต่ำ คู่-คี่ ประตูรวม เตะมุม 1×2 สองโอกาส ทายสกอร์ ครึ่งแรก-เต็มเวลา ประตูแรก ประตูสุดท้าย ทายแชมป์ UFABET มีรูปแบบพนันบอลออนไลน์มากกว่า SBOBET แทงบอลเดี่ยว แทงสเต็ป มิกซ์พาเลย์ สเต็ปคอมโบ เริ่มพนันบอลออนไลน์ที่ 2-12 คู่ ราคาน้ำดีที่สุด เปิดราคาเดิมพันครบทุกกีฬา บาสเก็ตบอล เทนนิส อเมริกันฟุตบอล ฮอกกี้น้ำแข็ง รักบี้ แฮนด์บอล แบดมินตัน วอลเลย์บอล มวยไทยสเต็ป มวยไทยสด ราคายกต่อยก
+
+  <img src="/img/1 (2).gif" alt="PG Slot"  />
+</p>
+<img src="/all/logo.png" alt="PG Slot"  />
+
+<h1>PG Gameเว็บตรง  คาสิโนสด คาสิโนออนไลน์</h1>
+<p className="w600">
+
+  เซ็กซี่เกมมิ่ง (เซ็กซี่ บาคาร่า) AE Casino, AE Sexy, Ae Seven บาคาร่าสด, บาคาร่าประกันภัย, เสือ-มังกร, ไฮโลสด, รูเล็ตสด เล่นคาสิโนสด ให้ค่าคอม 0.7% SA Gaming (เอสเอ เกมมิ่ง) คืนค่าคอม 0.7% เมื่อเล่นบาคาร่าออนไลน์ Squeeze Baccarat, Speed Baccarat, Money Wheel (วงล้อมหาโชค), สล็อตออนไลน์ Venus Casino (วีนัส คาสิโน) คาสิโนออนไลน์ที่ให้บริการ น้ำเต้าปูปลาและเกมคาสิโน เช่น Live Baccarat, Dragon Tiger, Sic Bo, Roulette GDG (Gold Diamond Gamong) คืนค่าคอม 0.7% ให้สมาชิกยูฟ่าเบทเมื่อเล่นบาคาร่า สล็อตออนไลน์ Evolution Gaming, eBET, BG Casino (Big Gaming), WE Casino (World Entertainment), BBIN LIVE และคาสิโนสดอีกหลายค่ายที่ UFABET เว็บตรงคาสิโนออนไลน์ที่ดีที่สุด
+  
+  
+  <img src="/img/1 (3).gif" alt="PG Slot"  />
+</p>
+
+<img src="/all/logo.png" alt="PG Slot"  />
+
+<h1>PG Gameเว็บตรง เกมฮอล สล็อต เกมยิงปลา</h1>
+<p className="w600">
+
+
+  PG Gameเว็บตรง (ยูฟ่าสล็อต) เกมยิงปลา พนันพื้นบ้านยอดนิยม เช่น ป๊อกเด้ง ไพ่แคง น้ำเต้าปูปลา 11ไฮโลโยนเหรียญ ม้าแข่ง เกมไพ่บาคาร่าออนไลน์ สล็อตแจ็คพอตสูงสุด หลักล้านบาท แตกบ่อยทุกเดือน Joker Gaming เกมยิงปลา 15 แบบ สล็อตมีโบนัส เสือ-มังกร Belangkai บาคาร่า Hulu Sicbo และเกมไพ่อื่นๆ PG SLOT (Pocket Game Soft) สล็อตเว็บตรงที่ UFABET เล่นได้ถอนเงินได้ไม่มีอั้น PP SLOT (Pragmatic Play) ค่ายสล็อตสุดฮิต 888Gold, Luck Dragons, Gold Train, Lady of Moon, Diamond Strike เกมสล็อตยอดนิยม สล็อตแตกง่าย Spadegaming ให้บริการสล็อตออนไลน์หลายรูปแบบ 1 – 100 Lines เกมสล็อตใหม่ๆ ที่มีแจ็กพอตรางวัลใหญ่รออยู่มากมาย RT SLOT (Red Tiger) รูเล็ต บาคาร่า สล็อตกว่า 50 แบบ เกมใหม่อัพเดททุกวัน KM (King Maker Casino) ไฮโลไทย น้ำเต้าปูปลา เสือ-มังกร กำถั่ว Belangkai ลูกเต๋าสร้างรายได้กับมือถือ ลงทุนเสี่ยงดวงเพียง 1 บาท CQ9, BNG, UPG, AUG, MT, VN, BBIN, JILI, Fa Chai, Microgaming, Genesis, NEXTSPIN, AE Slot, AE Gaming, PLAY8, ค่ายสล็อต ITP และ สล็อต UFABET ห้อง Game Hall รวมค่ายสล็อตเว็บตรง เล่นได้มากกว่าเว็บพนันออนไลน์อื่นๆ
+
+  <img src="/img/1 (1).jpg" alt="PG Slot"  />
+</p>
+<img src="/all/logo.png" alt="PG Slot"  />
+
+<h1> PG Gameเว็บตรง หวย ไก่ชน มวยสเต็ป Virtual Sport</h1>
+<p className="w600">
+
+
+  PG Gameเว็บตรง LOTTO หวย PG Gameเว็บตรง ไม่ผ่านเอเย่นต์ จ่ายบาทละ 900 หวยไทย หวยลาว หวยฮานอย หวยมาเลย์ หวยสิงคโปร์ หวยยี่กี (YeeKee) 4D, 3D, 2D, 1D ซื้อหวยได้ทุกรูปแบบ S-Lotto (สยามล็อตโต้) หวยรายวัน หวยวิ่ง 2ตัว 3ตัว ออกทุวัน หวยวิ่ง ลด 10%, หวย 2 ตัว ลด 25% หวย 3 ตัว ลด 33% ถ่ายทอดสดจากสถานที่จริงผ่าน Youtube เล่นง่ายจ่ายจริง แทงไก่ชนออนไลน์ (Cock Fighting) ตีไก่ไทย เวียดนาม ฟิลิปปินส์ ไก่ชนอินเตอร์ SV388 ถ่ายทอดสด พร้อมการเดิมพันออนไลน์บนมือถือและคอมพิวเตอร์ แทงมวยไทยออนไลน์ แทงมวยสเต็ป (Muay UFA, MuayStep2) เปิดราคามวยไทย เรตมวยยกต่อยก น่าเล่น ดูมวยถ่ายทดสด จากสนามเวทีมวยทั่วประเทศไทย UFABET เข้ามาเดิมพันออนไลน์ที่นี่มีราคา มวย วัวชน ไก่ชน ตะกร้อ E-Sport Snooker ให้บริการพนันออนไลน์และถ่ายทอดสด ดูฟรีทุกการแข่งขัน UFA ESPORT (อีสปอร์ต) เข้าสู่ระบบยูฟ่าเบท เว็บพนันออนไลน์ที่ดีที่สุด ในการแทงอีสปอร์ต เช่น ROV PUBG และอื่นๆ ที่มีตัวเลือกเดิมพันกับคู่แข่งขันทุกเกมส์มันส์ๆ Virtual Sport (เวอร์ช่วลสปอร์ต) SABA Sports, Virtual Football, Horse Racing RCB988 ไม่ต้องรอการแข่งขันสด ก็เข้ามาเดิมพันกีฬาเสมือนจริง ได้ตลอด 24 ชั่วโมง
+
+  <img src="/img/1 (2).png" alt="PG Slot"  />
+</p> 
+
+
+
+
+
+<div className="web">
+<img src="/all/112-1.png" alt="PG Slot"  />
+<img src="/all/092-1.png" alt="PG Slot"  />
+<img src="/all/082-1.png" alt="PG Slot"  />
+<img src="/all/122-1.png" alt="PG Slot"  />
+<img src="/all/072-1.png" alt="PG Slot"  />
+
+</div>
+<div className="webx">
+<img src="/all/05-1.png" alt="PG Slot"  />
+<img src="/all/06-1.png" alt="PG Slot"  />
+<img src="/all/10-1.png" alt="PG Slot"  />
+<img src="/all/014-1.png" alt="PG Slot"  />
+<img src="/all/052-1.png" alt="PG Slot"  />
+<img src="/all/03-1-1.png" alt="PG Slot"  />
+</div> 
+</main>
+
+
+
+  
+
+<div className="x-button-actions" id="account-actions-mobile">
+      <div className="-outer-wrapper">
+        <div className="-left-wrapper">
+          <span className="-item-wrapper">
+            <span className="-ic-img">
+              <span className="-textfooter d-block"><a href="#">เข้าสู่ระบบ</a></span>
+              <a href="#"><img width="300" height="300" src="all/login01-300x300.png" className="image wp-image-5207  attachment-medium size-medium" alt="" style={{ maxWidth: '100%', height: 'auto' }} decoding="async" loading="lazy" /></a>
+            </span>
+          </span>
+          <span className="-item-wrapper">
+            <span className="-ic-img">
+              <span className="-textfooter d-block"><a href="#">สมัครสมาชิก</a></span>
+              <a href="#"><img width="300" height="300" src="/all/register01-300x300.png" className="image wp-image-5208  attachment-medium size-medium" alt="" style={{ maxWidth: '100%', height: 'auto' }} decoding="async" loading="lazy" /></a>
+            </span>
+          </span>
+        </div>
+        <span className="-center-wrapper js-footer-lobby-selector js-menu-mobile-container">
+          <div className="-selected">
+            <h2 className="widgettitle"><a href="#">เข้าเล่น</a></h2>
+            <a href="#"><img width="380" height="295" src="all/ezgif-4-605b28d0c8-1.gif" className="image wp-image-5209  attachment-full size-full" alt="" style={{ maxWidth: '100%', height: 'auto' }} title="เข้าเล่น" decoding="async" loading="lazy" /></a>
+          </div>            
+        </span>
+        <div className="-fake-center-bg-wrapper">
+          <svg viewBox="-10 -1 30 12">
+            <defs>
+              <linearGradient id="rectangleGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                <stop offset="0%" stopColor="#2f2f2f"></stop>
+                <stop offset="100%" stopColor="#101010"></stop>
+              </linearGradient>
+            </defs>
+            <path d="M-10 -1 H30 V12 H-10z M 5 5 m -5, 0 a 5,5 0 1,0 10,0 a 5,5 0 1,0 -10,0z"></path>
+          </svg>
+        </div>
+        <div className="-right-wrapper">
+          <span className="-item-wrapper">
+            <span className="-ic-img">
+              <span className="-textfooter d-block"><a href="https://google.com/">แนะนำเพื่อน</a></span>
+              <img width="300" height="300" src="all/icon111-768x768-1-300x300.png" className="image wp-image-5211  attachment-medium size-medium" alt="" style={{ maxWidth: '100%', height: 'auto' }} decoding="async" loading="lazy" />
+            </span>
+          </span>
+          <span className="-item-wrapper">
+            <span className="-ic-img">
+              <span className="-textfooter d-block"><a href="#">ติดต่อเรา</a></span>
+              <a href="#"><img width="500" height="500" src="all/lineneon.png" className="image wp-image-5212  attachment-full size-full" alt="" style={{ maxWidth: '100%', height: 'auto' }} decoding="async" loading="lazy" /></a>
+            </span>
+          </span>
+        </div>
+        <div className="-fully-overlay js-footer-lobby-overlay"></div>
+      </div>
+    </div>
+
+
+
+    </>
+  );
+}
